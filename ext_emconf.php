9f6b2594a74177995d12ab04177442c7aa48f507
Pascal Mayer <laxap4@gmail.com>
2015-07-19 16:25:45 +0200

    Support TYPO3 7.3.x,

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,34 +1,39 @@
 <?php
 
 /***************************************************************
- * Extension Manager/Repository config file for ext: "bootstrap_links"
+ * Extension Manager/Repository config file for ext "bootstrap_links".
+ *
+ * Auto generated 14-05-2015 12:14
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Insert Link Extension for Bootstrap',
 	'description' => 'Adds another tab to the insert link window for bootstrap button links, popovers and links for opening modal boxes.',
 	'category' => 'misc',
+	'version' => '0.5.5',
+	'state' => 'beta',
+	'uploadfolder' => false,
+	'createDirs' => '',
+	'clearcacheonload' => true,
 	'author' => 'Pascal Mayer',
 	'author_email' => 'typo3@bsdist.ch',
 	'author_company' => '',
-	'shy' => '',
-	'version' => '0.5.4',
-	'priority' => '',
-	'module' => '',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
-	'lockType' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-7.1.99',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.2.0-7.3.99',
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
 		),
 	),
 );
 
-?>
