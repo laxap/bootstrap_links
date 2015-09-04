40e53ce0523921dec10a98faac392457599929e0
Pascal Mayer <laxap4@gmail.com>
2015-09-04 22:20:33 +0200

    Copyright

--- Classes/Hooks/ElementBrowser.php
+++ Classes/Hooks/ElementBrowser.php
@@ -1,35 +1,24 @@
 <?php
 namespace Laxap\BootstrapLinks\Hooks;
 
-	/***************************************************************
-	 *  Copyright notice
-	 *
-	 *  (c) 2015 Pascal Mayer <typo3(a)bsdist.ch>
-	 *
-	 *  All rights reserved
-	 *
-	 *  This script is part of the TYPO3 project. The TYPO3 project is
-	 *  free software; you can redistribute it and/or modify
-	 *  it under the terms of the GNU General Public License as published by
-	 *  the Free Software Foundation; either version 3 of the License, or
-	 *  (at your option) any later version.
-	 *
-	 *  The GNU General Public License can be found at
-	 *  http://www.gnu.org/copyleft/gpl.html.
-	 *
-	 *  This script is distributed in the hope that it will be useful,
-	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 *  GNU General Public License for more details.
-	 *
-	 *  This copyright notice MUST APPEAR in all copies of the script!
-	 ***************************************************************/
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
 /**
  * Link wizard handler for twitter bootstrap elements.
 
  * @package BootstrapLinks
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ * @author Pascal Mayer <typo3@bsdist.ch>
  */
 class ElementBrowser implements \TYPO3\CMS\Core\ElementBrowser\ElementBrowserHookInterface {
 
