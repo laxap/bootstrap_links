c0a1bd5b2f64050cf59c3e763ae2f126edfbe584
Pascal Mayer <laxap4@gmail.com>
2016-01-23 23:09:52 +0100

    Config option to add bootstrap button styles to default link styles

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,8 +9,6 @@ if ( strlen($_EXTCONF) ) {
 
 if ( TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 7000000 ) {
 	// --- Hooks in sysext/core/Classes/Html/RteHtmlParser.php ---
-	// Add linkhandler
-	// additional available hooks: removeParams($parameters, $this), modifyParamsLinksDb($p, $this), modifyParamsLinksRte($p, $this)
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typolinkLinkHandler']['bootstrap']  = 'EXT:bootstrap_links/Classes/Hooks/LinkHandler.php:&Laxap\BootstrapLinks\Hooks\LinkHandler';
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_parsehtml_proc.php']['modifyParams_LinksRte_PostProc'][] = 'EXT:bootstrap_links/Classes/Hooks/LinkHandler.php:&Laxap\BootstrapLinks\Hooks\LinkHandler';
 	// --- Hook in sysext/rtehtmlarea/Classes/BrowseLinks.php
@@ -19,7 +17,11 @@ if ( TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.browse_links.php']['browseLinksHook'][] = 'EXT:bootstrap_links/Classes/Hooks/ElementBrowser.php:Laxap\BootstrapLinks\Hooks\ElementBrowser';
 
 } else {
-
+	// --- Hook in TYPO3\CMS\Core\Html\RteHtmlParser.php and TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer ---
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typolinkLinkHandler']['bootstrap']  = 'EXT:bootstrap_links/Classes/Hooks/LinkHandler.php:&Laxap\BootstrapLinks\Hooks\LinkHandler';
+
+	if ( isset($extConf['addBootstrapLinkStyles']) && $extConf['addBootstrapLinkStyles'] ) {
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_links/Configuration/TypoScript/tsconfig.ts">');
+	}
 }
 ?>
