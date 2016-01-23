4b07f2e3d057d8a52b83d175b4546b47893f542a
Pascal Mayer <laxap4@gmail.com>
2016-01-23 22:51:41 +0100

    Copyright text

--- Classes/Hooks/ElementBrowser.php
+++ Classes/Hooks/ElementBrowser.php
@@ -2,8 +2,6 @@
 namespace Laxap\BootstrapLinks\Hooks;
 
 /*
- * This file is part of the TYPO3 CMS project.
- *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
  * of the License, or any later version.
@@ -11,7 +9,6 @@ namespace Laxap\BootstrapLinks\Hooks;
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  *
- * The TYPO3 project - inspiring people to share!
  */
 
 /**
